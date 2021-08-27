f106b142d328e2db2d0774cd243ab4f7c5096ca4
Gizem <mgizemykbg@gmail.com>
2021-08-27 22:49:24 +0300

    edited app.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import "./App.css";
 
 import AdminPage from "./pages/admin-page";
 import LoginPage from "./pages/login-page";
+import FormPage from "./pages/form-page";
 
 function App() {
   const [admin, setAdmin] = useLocalStorage("user-data", "");
@@ -18,6 +19,10 @@ function App() {
     <>
       <Router>
         <Switch>
+          <Route path="/" exact>
+            <FormPage />
+          </Route>
+
           <Route path="/AdminPage">
             {admin ? <AdminPage /> : <LoginPage {...{ setAdmin }} />}
           </Route>
---
+++ src/pages/form-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ApplyForm } from "../components/apply-form";
+import Navbar from "../components/navbar/navbar";
+
+function FormPage() {
+  return (
+    <div>
+      <Navbar />
+      <ApplyForm />
+    </div>
+  );
+}
+
+export default FormPage;
