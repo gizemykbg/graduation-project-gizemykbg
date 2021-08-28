8ae29e4d71c66a8ad22931533ba272d82536ff54
Gizem <mgizemykbg@gmail.com>
2021-08-29 01:25:24 +0300

    added succesPage

--- src/components/apply-form.js
+++ src/components/apply-form.js
@@ -5,7 +5,7 @@ import { TextField } from "./textfield";
 import * as Yup from "yup";
 
 import { addForm } from "../firebase/services";
-export const ApplyForm = ({}) => {
+export const ApplyForm = ({ applyValue }) => {
   const [loading, setLoading] = useState("");
   const history = useHistory();
 
@@ -18,7 +18,9 @@ export const ApplyForm = ({}) => {
       .required(),
     age: Yup.number().required(),
     content: Yup.string().required(),
-    tc: Yup.number().min(11, "Must be 11 characters").required(), // ??? salladm.
+    tc: Yup.string()
+      .matches(/^[0-9]{11}$/, "Bu alana 11 haneli kimlik numaranızı giriniz.")
+      .required(),
     adress: Yup.string().required().max(45, ".."),
     cover: Yup.mixed().test(
       "cover",
@@ -46,11 +48,11 @@ export const ApplyForm = ({}) => {
       onSubmit={(values) => {
         console.log(values);
         addForm(values)
-          .then((values) => {
-            console.log("sıkujd");
-            history.push({
+          .then((applyId) => {
+            console.log(applyId);
+            history.replace({
               pathname: "/basvuru-basarili",
-              state: { values, dataid: values },
+              state: { applyId },
             });
           })
           .catch((error) => {
@@ -62,13 +64,48 @@ export const ApplyForm = ({}) => {
         <div>
           <h1>Başvuru Formu</h1>
           <Form>
-            <TextField label="First Name" name="firstname" type="text" />
-            <TextField label="Last Name" name="lastname" type="text" />
-            <TextField label="Age" name="age" type="number" />
-            <TextField label="Açıklama" name="content" type="text" />
-            <TextField label="Tc No:" name="tc" type="number" />
-            <TextField label="Adress" name="adress" type="text" />
-            <TextField label="dosya" type="file" name="cover" />
+            <TextField
+              label="First Name"
+              name="firstname"
+              type="text"
+              value={applyValue && applyValue.firstname}
+            />
+            <TextField
+              label="Last Name"
+              name="lastname"
+              type="text"
+              value={applyValue && applyValue.lastname}
+            />
+            <TextField
+              label="Age"
+              name="age"
+              type="number"
+              value={applyValue && applyValue.age}
+            />
+            <TextField
+              label="Açıklama"
+              name="content"
+              type="text"
+              value={applyValue && applyValue.content}
+            />
+            <TextField
+              label="Tc No:"
+              name="tc"
+              type="number"
+              value={applyValue && applyValue.tc}
+            />
+            <TextField
+              label="Adress"
+              name="adress"
+              type="text"
+              value={applyValue && applyValue.adress}
+            />
+            <TextField
+              label="dosya"
+              type="file"
+              name="cover"
+              value={applyValue && applyValue.file}
+            />
             <button className="btn" type="submit">
               Gönder
             </button>
--- src/firebase/services.js
+++ src/firebase/services.js
@@ -1,4 +1,5 @@
 import firebase, { firestore } from "./config";
+//import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const formRef = firestore.collection("forms");
 
@@ -14,6 +15,7 @@ export const addForm = async (data) => {
       adress: data.adress,
       cover: data.cover[0],
       status: "Pending",
+      adminMessage: "",
       createdDate: firebase.firestore.Timestamp.now(),
     })
     .then((docs) => {
@@ -22,3 +24,162 @@ export const addForm = async (data) => {
 
   return id;
 };
+
+export const getData = async (formId) => {
+  let formData;
+  console.log(formData);
+  await formRef
+    .doc(formId)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        formData = doc.data();
+      } else {
+        formData = "";
+      }
+    })
+    .catch((error) => {
+      console.log("Error", error);
+    });
+
+  return formData;
+};
+
+export const adminFormList = (res) => {
+  const formList = [];
+  formRef
+    .get()
+    .then((snapShot) => {
+      snapShot.forEach((formDoc) => {
+        formList.push({ id: formDoc.id, ...formDoc.data() });
+      });
+      res(formList);
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const updateForm = (formId, data, res) => {
+  console.log(data);
+  const editFormRef = formRef.doc(formId);
+  editFormRef
+    .update({
+      status: data.status,
+      adminMessage: data.adminMessage,
+    })
+    .then((doc) => {
+      console.log(doc);
+      res(doc);
+    });
+};
+
+/* export const updateForm = async (formId, data) => {
+  await formRef
+    .doc(formId)
+    .update({
+      status: data.status,
+      adminMessage: data.adminMessage,
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}; 
+export async function createForm(url, form) {
+  const fileRef = await storage.refFromURL(url);
+  var d;
+  var newForm = {
+    firstname: form.firstname,
+    lastname: form.lastname,
+    age: form.age,
+    tc: form.tc,
+    adress: form.adress,
+    cover: url,
+    fileRef: fileRef.location.path,
+  };
+  return firestore.collection("forms").add(newForm);
+}
+export const AddForm = async (data) => {
+  console.log(data);
+  // history.push({ });success yap
+  let d;
+  let form = {
+    firstname: data.firstname,
+    lastname: data.lastname,
+    age: data.age,
+    tc: data.tc,
+    adress: data.adress,
+    cover: data.cover[0],
+  };
+
+  var storageRef = firebase.storage().ref();
+  var storageChild = storageRef.child(form.cover.name);
+  var formCover = storageChild.put(form.cover);
+
+  await new Promise((resolve) => {
+    formCover.on(
+      "state_changed",
+      (snapshot) => {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        //setLoading(Math.trunc(progress));
+      },
+      (error) => {
+        console.log(error);
+      },
+      async () => {
+        var dowloadURL = await storageChild.getDownloadURL();
+        d = dowloadURL;
+        console.log(d);
+        resolve();
+      }
+    );
+  });
+
+  firebase
+    .createForm(d, form)
+    .then((form) => {
+      console.log("form created succesfully", form); //success page
+      getForms();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+/* 
+export async function AddRes(formId, message) {
+  const formRef = db.collection("forms");
+  await formRef
+    .docRef(formId)
+    .collection("messages")
+    .add({
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      message,
+      //senderID: admin,
+    })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+} */
+
+/* 
+export function getFormData(adminName) {
+  const formRef = db.collection("forms"); //admin
+
+  return formRef
+    .where("name", "==", adminName)
+    .get()
+    .then((querySnapshot) => querySnapshot.docs.map((doc) => doc.data())[0]);
+}
+
+export function updateForm(formId, setter, adminId) {
+  const formRef = db.collection("forms");
+  const resRef = formRef.doc(adminId).collection("messages");
+}
+  */
+/*export async function getAllForms() {
+  return firestore.collection("forms").get();
+}*/
